3d528a6c73a7db72e0da17c70dbee23f2a83d1e9
konishi333 <jobhuntingkonishi3@gmail.com>
2025-01-19 20:34:57 +0900

    タイトル名変更

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Recorder from "@/components/recorder";
 import List from "@/components/list";
@@ -10,9 +9,17 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <Stack.Navigator initialRouteName="Recorder">
-      <Stack.Screen name="Recorder" component={Recorder} />
-      <Stack.Screen name="List" component={List} />
-      <Stack.Screen name="VideoPlayer" component={VideoPlayer} />
+      <Stack.Screen
+        name="Recorder"
+        component={Recorder}
+        options={{ title: "録画" }}
+      />
+      <Stack.Screen name="List" component={List} options={{ title: "一覧" }} />
+      <Stack.Screen
+        name="VideoPlayer"
+        component={VideoPlayer}
+        options={{ title: "視聴" }}
+      />
     </Stack.Navigator>
   );
 }
